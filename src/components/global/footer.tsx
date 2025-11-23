27b178ebd05275be0bf20cd18a7852545d1481c5
Ddm Martin <martinddm301@gmail.com>
2025-11-23 07:19:08 +0000

    voici le lien snapchat du site https://snapchat.com/t/qez0Z1z0

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -6,6 +6,24 @@ import Link from 'next/link';
 import { useTranslation } from '@/hooks/use-translation';
 import { Separator } from '@/components/ui/separator';
 
+const SnapchatIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M12 4.1c-5.9 0-8.9 5.8-8.9 9.9 0 4.1 3 9.9 8.9 9.9s8.9-5.8 8.9-9.9c0-4.1-3-9.9-8.9-9.9zm0 14.9c-2.8 0-5-2.2-5-5s2.2-5 5-5 5 2.2 5 5-2.2 5-5 5zm-4.3-5.3c.2-.2.5-.3.8-.3s.6.1.8.3c.2.2.3.5.3.8s-.1.6-.3.8c-.2.2-.5.3-.8.3s-.6-.1-.8-.3c-.2-.2-.3-.5-.3-.8s.1-.6.3-.8z" />
+    </svg>
+);
+
+
 export function Footer() {
   const { t } = useTranslation();
 
@@ -13,6 +31,7 @@ export function Footer() {
     { name: 'Twitter', href: '#', icon: Twitter },
     { name: 'Facebook', href: '#', icon: Facebook },
     { name: 'Instagram', href: '#', icon: Instagram },
+    { name: 'Snapchat', href: 'https://snapchat.com/t/qez0Z1z0', icon: SnapchatIcon },
   ];
 
   return (
