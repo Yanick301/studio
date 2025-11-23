28f86c6734546bfe0db733b7e853aa09180433ca
Ddm Martin <martinddm301@gmail.com>
2025-11-23 05:18:18 +0000

    analyse tout le projet chaque fichier chaque dossier et corrige moi tout

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'i.pravatar.cc',
         port: '',
         pathname: '/**',
